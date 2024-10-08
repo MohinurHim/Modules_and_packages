e42bf84de1bbdd48d289b745fc5ee80dc1fb2945
MohinurHim <himmatovamohinur@gmail.com>
2024-10-08 14:33:38 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def fake_divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        result = first / second
+        return result
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+print(result1)
+print(result2)
---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+# Модули и пакеты. А как делить?
+from fake_math import fake_divide as f_m, result1
+from true_math import true_divide as t_m
+
+print(f_m(69,3 ))
+print(f_m(3, 0))
+print(t_m(49, 7))
+print(t_m(15, 0))
